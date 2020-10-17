29522b6508e4efc66e59ba3dcaef494eb7325010
Owner <owner@Owners-MacBook-Air.local>
2020-10-17 13:24:24 -0400

    flash

---
+++ app.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, render_template
+
+
+#################################################
+# Database Setup
+#################################################
+#engine = create_engine("sqlite:///titanic.sqlite")
+
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(engine, reflect=True)
+
+# # Save reference to the table
+# brewers = Base.classes.brewers
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/state")
+def state():
+    return render_template("state.html")
+
+@app.route("/find")
+def find():
+    return render_template("find.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run()
+

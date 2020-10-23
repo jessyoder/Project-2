888a1655744e4897eb9841dead4ba75e3efac778
Todd Schanzlin <toddschanzlin@yahoo.com>
2020-10-23 16:20:07 -0400

    Friday upload 2

--- Mapping/static/js/index_charts.js
+++ Mapping/static/js/index_charts.js
@@ -7,18 +7,36 @@ var margin = { top: 20, right: 30, bottom: 30, left: 55 },
 // Chart 1 - Total US Beer Production by Size of Brewery
 // --------------------------------------------------------------------------------
 // append the svg object to the body of the page
-var svg1 = d3.select("#my_dataviz1")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+// var svg1 = d3.select("#my_dataviz1")
+//     .append("svg")
+//     .attr("width", width + margin.left + margin.right)
+//     .attr("height", height + margin.top + margin.bottom)
+//     .append("g")
+//     .attr("transform",
+//         "translate(" + margin.left + "," + margin.top + ")");
 
-//Read the data
-d3.json("/api/data1", function (data) {
+// //Read the data from the JSON file
+// d3.json("/api/data1", function (data) {
 
-    console.log("Hello")
-    //console.log(data.Chart1)
+//     console.log("Hello");
+//     console.log(data[0]);
+//     console.log(data[0].Chart1)
 
-});
+//     var chartdata = data[0].Chart1
+
+//     // for (var i = 0; i < chartdata.length; i++) {
+//     //     if (i = 0) {
+//     //         labels = Object.keys(chartdata)
+//     //         values 
+//     //         console.log(labels)
+//     //     }
+
+//     // };
+
+// });
+
+// console.log(chartdata.map)
+
+
+
+
